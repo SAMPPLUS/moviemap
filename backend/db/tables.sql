6501dcbbdf6e56ddd298d8cc9a03cfb030b8e95a
sam-pete <sampeterson560@gmail.com>
2023-11-13 17:36:40 -0700

    new db backup

--- backend/db/tables.sql
+++ backend/db/tables.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 16.0
 -- Dumped by pg_dump version 16.0
 
--- Started on 2023-11-05 14:37:49
+-- Started on 2023-11-13 17:35:53
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -20,16 +20,14 @@ SET row_security = off;
 
 --
 -- TOC entry 6 (class 2615 OID 2200)
--- Name: public; Type: SCHEMA; Schema: -; Owner: sampeterson
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
 --
 
 -- *not* creating schema, since initdb creates it
 
 
-ALTER SCHEMA public OWNER TO sampeterson;
-
 --
--- TOC entry 2 (class 3079 OID 17585)
+-- TOC entry 2 (class 3079 OID 19871)
 -- Name: postgis; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -37,9 +35,9 @@ CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
 
 
 --
--- TOC entry 5798 (class 0 OID 0)
+-- TOC entry 5808 (class 0 OID 0)
 -- Dependencies: 2
--- Name: EXTENSION postgis; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION postgis; Type: COMMENT; Schema: -; Owner: -
 --
 
 COMMENT ON EXTENSION postgis IS 'PostGIS geometry and geography spatial types and functions';
@@ -50,8 +48,47 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 221 (class 1259 OID 18661)
--- Name: locations; Type: TABLE; Schema: public; Owner: sampeterson
+-- TOC entry 221 (class 1259 OID 20947)
+-- Name: locationimages; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.locationimages (
+    id integer NOT NULL,
+    location_id integer,
+    file_name text,
+    type integer,
+    description character varying(240),
+    created_at timestamp without time zone DEFAULT now(),
+    CONSTRAINT locationimages_type_check CHECK (((type = 1) OR (type = 2)))
+);
+
+
+--
+-- TOC entry 222 (class 1259 OID 20953)
+-- Name: locationimages_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.locationimages_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 5809 (class 0 OID 0)
+-- Dependencies: 222
+-- Name: locationimages_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.locationimages_id_seq OWNED BY public.locationimages.id;
+
+
+--
+-- TOC entry 223 (class 1259 OID 20954)
+-- Name: locations; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.locations (
@@ -61,15 +98,15 @@ CREATE TABLE public.locations (
     "time" timestamp without time zone,
     submitted_user integer,
     geo public.geography(Point,4326),
-    title character varying(255)
+    title character varying(255),
+    sequence_order integer,
+    main_img_path text
 );
 
 
-ALTER TABLE public.locations OWNER TO sampeterson;
-
 --
--- TOC entry 222 (class 1259 OID 18666)
--- Name: locations_id_serial; Type: SEQUENCE; Schema: public; Owner: sampeterson
+-- TOC entry 224 (class 1259 OID 20959)
+-- Name: locations_id_serial; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE public.locations_id_serial
@@ -81,20 +118,18 @@ CREATE SEQUENCE public.locations_id_serial
     CACHE 1;
 
 
-ALTER SEQUENCE public.locations_id_serial OWNER TO sampeterson;
-
 --
--- TOC entry 5799 (class 0 OID 0)
--- Dependencies: 222
--- Name: locations_id_serial; Type: SEQUENCE OWNED BY; Schema: public; Owner: sampeterson
+-- TOC entry 5810 (class 0 OID 0)
+-- Dependencies: 224
+-- Name: locations_id_serial; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.locations_id_serial OWNED BY public.locations.id;
 
 
 --
--- TOC entry 223 (class 1259 OID 18667)
--- Name: movies; Type: TABLE; Schema: public; Owner: sampeterson
+-- TOC entry 225 (class 1259 OID 20960)
+-- Name: movies; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.movies (
@@ -108,11 +143,9 @@ CREATE TABLE public.movies (
 );
 
 
-ALTER TABLE public.movies OWNER TO sampeterson;
-
 --
--- TOC entry 224 (class 1259 OID 18672)
--- Name: movies_id_serial; Type: SEQUENCE; Schema: public; Owner: sampeterson
+-- TOC entry 226 (class 1259 OID 20965)
+-- Name: movies_id_serial; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE public.movies_id_serial
@@ -124,20 +157,18 @@ CREATE SEQUENCE public.movies_id_serial
     CACHE 1;
 
 
-ALTER SEQUENCE public.movies_id_serial OWNER TO sampeterson;
-
 --
--- TOC entry 5800 (class 0 OID 0)
--- Dependencies: 224
--- Name: movies_id_serial; Type: SEQUENCE OWNED BY; Schema: public; Owner: sampeterson
+-- TOC entry 5811 (class 0 OID 0)
+-- Dependencies: 226
+-- Name: movies_id_serial; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.movies_id_serial OWNED BY public.movies.id;
 
 
 --
--- TOC entry 225 (class 1259 OID 18673)
--- Name: test; Type: TABLE; Schema: public; Owner: sampeterson
+-- TOC entry 227 (class 1259 OID 20966)
+-- Name: test; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.test (
@@ -147,11 +178,9 @@ CREATE TABLE public.test (
 );
 
 
-ALTER TABLE public.test OWNER TO sampeterson;
-
 --
--- TOC entry 226 (class 1259 OID 18678)
--- Name: test_id_seq; Type: SEQUENCE; Schema: public; Owner: sampeterson
+-- TOC entry 228 (class 1259 OID 20971)
+-- Name: test_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE public.test_id_seq
@@ -163,20 +192,18 @@ CREATE SEQUENCE public.test_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.test_id_seq OWNER TO sampeterson;
-
 --
--- TOC entry 5801 (class 0 OID 0)
--- Dependencies: 226
--- Name: test_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: sampeterson
+-- TOC entry 5812 (class 0 OID 0)
+-- Dependencies: 228
+-- Name: test_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.test_id_seq OWNED BY public.test.id;
 
 
 --
--- TOC entry 227 (class 1259 OID 18679)
--- Name: users; Type: TABLE; Schema: public; Owner: sampeterson
+-- TOC entry 229 (class 1259 OID 20972)
+-- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.users (
@@ -187,35 +214,50 @@ CREATE TABLE public.users (
 );
 
 
-ALTER TABLE public.users OWNER TO sampeterson;
+--
+-- TOC entry 5631 (class 2604 OID 20977)
+-- Name: locationimages id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.locationimages ALTER COLUMN id SET DEFAULT nextval('public.locationimages_id_seq'::regclass);
+
 
 --
--- TOC entry 5626 (class 2604 OID 18684)
--- Name: locations id; Type: DEFAULT; Schema: public; Owner: sampeterson
+-- TOC entry 5633 (class 2604 OID 20978)
+-- Name: locations id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.locations ALTER COLUMN id SET DEFAULT nextval('public.locations_id_serial'::regclass);
 
 
 --
--- TOC entry 5627 (class 2604 OID 18685)
--- Name: movies id; Type: DEFAULT; Schema: public; Owner: sampeterson
+-- TOC entry 5634 (class 2604 OID 20979)
+-- Name: movies id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.movies ALTER COLUMN id SET DEFAULT nextval('public.movies_id_serial'::regclass);
 
 
 --
--- TOC entry 5628 (class 2604 OID 18686)
--- Name: test id; Type: DEFAULT; Schema: public; Owner: sampeterson
+-- TOC entry 5635 (class 2604 OID 20980)
+-- Name: test id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.test ALTER COLUMN id SET DEFAULT nextval('public.test_id_seq'::regclass);
 
 
 --
--- TOC entry 5633 (class 2606 OID 18688)
--- Name: locations locations_pkey; Type: CONSTRAINT; Schema: public; Owner: sampeterson
+-- TOC entry 5641 (class 2606 OID 20982)
+-- Name: locationimages locationimages_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.locationimages
+    ADD CONSTRAINT locationimages_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 5643 (class 2606 OID 20984)
+-- Name: locations locations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.locations
@@ -223,8 +265,8 @@ ALTER TABLE ONLY public.locations
 
 
 --
--- TOC entry 5635 (class 2606 OID 18690)
--- Name: movies movies_pkey; Type: CONSTRAINT; Schema: public; Owner: sampeterson
+-- TOC entry 5645 (class 2606 OID 20986)
+-- Name: movies movies_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.movies
@@ -232,8 +274,8 @@ ALTER TABLE ONLY public.movies
 
 
 --
--- TOC entry 5637 (class 2606 OID 18692)
--- Name: movies movies_tmdb_id_key; Type: CONSTRAINT; Schema: public; Owner: sampeterson
+-- TOC entry 5647 (class 2606 OID 20988)
+-- Name: movies movies_tmdb_id_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.movies
@@ -241,8 +283,8 @@ ALTER TABLE ONLY public.movies
 
 
 --
--- TOC entry 5639 (class 2606 OID 18694)
--- Name: test test_pkey; Type: CONSTRAINT; Schema: public; Owner: sampeterson
+-- TOC entry 5649 (class 2606 OID 20990)
+-- Name: test test_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.test
@@ -250,8 +292,8 @@ ALTER TABLE ONLY public.test
 
 
 --
--- TOC entry 5641 (class 2606 OID 18696)
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: sampeterson
+-- TOC entry 5651 (class 2606 OID 20992)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users
@@ -259,8 +301,17 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 5642 (class 2606 OID 18697)
--- Name: locations locations_movie_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: sampeterson
+-- TOC entry 5652 (class 2606 OID 20993)
+-- Name: locationimages locationimages_location_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.locationimages
+    ADD CONSTRAINT locationimages_location_id_fkey FOREIGN KEY (location_id) REFERENCES public.locations(id);
+
+
+--
+-- TOC entry 5653 (class 2606 OID 20998)
+-- Name: locations locations_movie_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.locations
@@ -268,25 +319,15 @@ ALTER TABLE ONLY public.locations
 
 
 --
--- TOC entry 5643 (class 2606 OID 18702)
--- Name: locations locations_submitted_user_fkey; Type: FK CONSTRAINT; Schema: public; Owner: sampeterson
+-- TOC entry 5654 (class 2606 OID 21003)
+-- Name: locations locations_submitted_user_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.locations
     ADD CONSTRAINT locations_submitted_user_fkey FOREIGN KEY (submitted_user) REFERENCES public.users(id);
 
 
---
--- TOC entry 5797 (class 0 OID 0)
--- Dependencies: 6
--- Name: SCHEMA public; Type: ACL; Schema: -; Owner: sampeterson
---
-
-REVOKE USAGE ON SCHEMA public FROM PUBLIC;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
--- Completed on 2023-11-05 14:37:49
+-- Completed on 2023-11-13 17:35:53
 
 --
 -- PostgreSQL database dump complete
